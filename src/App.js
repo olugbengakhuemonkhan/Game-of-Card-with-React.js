d472fbf3682506e70fa6c6b47ca2da573d07758f
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2019-02-15 18:09:16 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PokerTable from './components/PokerTable';
+
+class App extends Component {
+  render() {
+    return(
+      <div className="container">
+        <PokerTable />
+      </div> 
+    )
+  }
+}
+
+export default App;
+
+
+
+
+
